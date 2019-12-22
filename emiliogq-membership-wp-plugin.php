47c0c141c4a1447ea5149bdca86cc453b9e93507
Emilio Garcia <emilio.garcia@ieec.cat>
2019-12-22 21:08:13 +0100

    Initial header file.

---
+++ emiliogq-membership-wp-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       Emiliogq Membership Wordpress Plugin
+ * Plugin URI:        http://web.emiliogq.com/projects/membership-wp-plugin/
+ * Description:       Wordpress plugin used for managing memberships.
+ * Version:           0.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Emilio Garcia
+ * Author URI:        http://web.emiliogq.com/
+ * Text Domain:       emiliogq-membership-wp-plugin
+ * Domain Path:       /languages
+ */
+?>
